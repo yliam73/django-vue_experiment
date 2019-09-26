8ff759d4c368c7438dd3fd795e25c9cc2af6f33a
John Gaebriel Vargas <jvargas@servio.ph>
2019-09-26 23:20:48 +0800

    Implement ASGI server for future compatibility and use. Updated gitignore

--- cms/settings.py
+++ cms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cms.wsgi.application'
+ASGI_APPLICATION = 'cms.routing.application'
 
 
 # Database
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cms',
+        'USER': 'cms_admin',
+        'PASSWORD': 'c1m2s3',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
